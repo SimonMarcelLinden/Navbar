48e128fd4f31b02637b94cc4bd301fdc0efedb81
SimonMarcelLinden <info@snerve.de>
2021-07-29 10:38:54 +0200

    Nav component

---
+++ src/lib-components/navbar-nav/navbar-nav.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+
+export default /*#__PURE__*/ Vue.extend({
+  name: "lNavbarNav",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+});
---
+++ src/lib-components/navbar-nav/navbar-nav.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="navbar-nav">
+    <template><slot /></template>
+  </ul>
+</template>
+
+<script src="./navbar-nav.ts" lang="ts"></script>
