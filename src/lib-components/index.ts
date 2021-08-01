48977cf45a98e0e750762e9c482e9d358e719fda
SimonMarcelLinden <info@snerve.de>
2021-08-01 13:29:00 +0200

    Fixed exported name for nav item

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export { default as lNavbar }       from '@/lib-components/navbar/navbar.vue';
 export { default as lNavbarNav }    from '@/lib-components/navbar-nav/navbar-nav.vue';
-export { default as LNavItem }      from '@/lib-components/nav-item/nav-item.vue';
+export { default as lNavItem }      from '@/lib-components/nav-item/nav-item.vue';
 export { default as lNavDropdown }  from '@/lib-components/nav-dropdown/nav-dropdown.vue';
 
 export { default as lNavBrand } from '@/lib-components/nav-brand/nav-brand.vue';
