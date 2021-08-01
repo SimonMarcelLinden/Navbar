9c0f0329e1ab1b5c999b1a04c0a1e9382581cc1c
SimonMarcelLinden <info@snerve.de>
2021-08-01 13:10:39 +0200

    Export nav-brand component

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -3,3 +3,5 @@ export { default as lNavbar }       from '@/lib-components/navbar/navbar.vue';
 export { default as lNavbarNav }    from '@/lib-components/navbar-nav/navbar-nav.vue';
 export { default as LNavItem }      from '@/lib-components/nav-item/nav-item.vue';
 export { default as lNavDropdown }  from '@/lib-components/nav-dropdown/nav-dropdown.vue';
+
+export { default as lNavBrand } from '@/lib-components/nav-brand/nav-brand.vue';
